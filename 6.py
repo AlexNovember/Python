77cf77d860b1ce5f091cf2eee413c204f65f2f0c
Alex <tanko77@rambler.ru>
2022-12-03 11:53:59 +0300

    5

--- 6.py
+++ 6.py
@@ -79,52 +79,36 @@
 # - для k = 8 список будет выглядеть 
 # так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-# def to_directions_fibonacci(num):
-#     fib = {-2: -1, -1: 1, 0: 0, 1: 1}
-#     i = 2
-#     while i <= num:
-#         fib.update({i: fib[i-2] + fib[i-1]})
-#         fib.update({-i: fib[-i+2] - fib[-i+1]})
-#         i += 1
-#     return fib
-
-# print(to_directions_fibonacci(8))
-
-
-# mem = {1: 1, 2: 1}
- 
- 
-# def fib(n):
-#     if n not in mem:
-#         mem[n] = fib(n - 2) - fib(n + 1)
- 
-#     return mem[n]
- 
- 
-# print(fib(8))
-# print(mem)
-
-
-
-
-def fib(n):
-    if n>=0:
-       id_x = range(n+1)
-       x = [0,1]
-       for k in id_x[2:]:
-           x.append(x[k-1] + x[k-2]) 
-       return x[n]
-    else:
-       n=-(n-1)
-       id_x = range(n+1)
-       x = [1,0]
-       for k in id_x[2:]:
-           x.append(x[k-2] - x[k-1]) 
-       x.reverse()
-    return x[0]
-
-
+def to_directions_fibonacci(num):
+    fib = {-2: -1, -1: 1, 0: 0, 1: 1}
+    i = 2
+    while i <= num:
+        fib.update({i: fib[i-2] + fib[i-1]})
+        fib.update({-i: fib[-i+2] - fib[-i+1]})
+        i += 1
+    return (fib)
 
 n = int(input("Введите число: "))
-for i in range(-n, n):
-   print(fib(i), end = " ")
+print(to_directions_fibonacci(n), end = " ")
+
+# def fib(n):
+#     if n>=0:
+#        id_x = range(n+1)
+#        x = [0,1]
+#        for k in id_x[2:]:
+#            x.append(x[k-1] + x[k-2]) 
+#        return x[n]
+#     else:
+#        n=-(n-1)
+#        id_x = range(n+1)
+#        x = [1,0]
+#        for k in id_x[2:]:
+#            x.append(x[k-2] - x[k-1]) 
+#        x.reverse()
+#     return x[0]
+
+
+
+# n = int(input("Введите число: "))
+# for i in range(-n, n):
+#    print(fib(i), end = " ")
